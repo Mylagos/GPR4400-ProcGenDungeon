e8bd1780a0a50efd65561f8aaa88eed6c869919c
Mylagos <Eddybo2001@gmail.com>
2022-05-21 17:22:04 +0200

    Rework player

--- Assets/02-Scripts/PathFinding/Astar.cs
+++ Assets/02-Scripts/PathFinding/Astar.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Astar : MonoBehaviour
+public class AStarPathFinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _nbIterationsMax;
+    [SerializeField]
+    [Range(0.1f, 2)] private float _heuristicFactor = 1f;
+
+    public void GeneratePath(List<Vector2Int> map, Vector2Int start, Vector2Int goal)
     {
-        
+
+    }
+}
+
+public class AStarNode
+{
+    private Vector2Int _nodePos;
+    private float _H;
+    private float _C;
+    private AStarNode _parent;
+
+    public float S { get { return _H + _C; } }
+
+    public float H { get => _H; set => _H = value; }
+    public float C { get => _C; set => _C = value; }
+    public AStarNode Parent { get => _parent; set => _parent = value; }
+    public Vector2Int Position { get => _nodePos; set => _nodePos = value; }
+
+
+    public AStarNode(Vector2Int pos, float h, float c, AStarNode parent)
+    {
+        _nodePos = pos;
+        _H = h;
+        _C = c;
+        _parent = parent;
     }
 
-    // Update is called once per frame
-    void Update()
+    public AStarNode(AStarNode copy)
     {
-        
+        _nodePos = copy.Position;
+        _H = copy._H;
+        _C = copy._C;
+        _parent = copy.Parent;
     }
+
 }
