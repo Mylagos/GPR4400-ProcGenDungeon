5c78c0666e4cdf84f8c2839f18ef23b4cabe28d7
Mylagos <Eddybo2001@gmail.com>
2022-05-29 19:47:39 +0200

    Astar Added

--- Assets/02-Scripts/PathFinding/Astar.cs
+++ Assets/02-Scripts/PathFinding/Astar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AStarPathFinder : MonoBehaviour
@@ -8,9 +9,62 @@ public class AStarPathFinder : MonoBehaviour
     [SerializeField]
     [Range(0.1f, 2)] private float _heuristicFactor = 1f;
 
-    public void GeneratePath(List<Vector2Int> map, Vector2Int start, Vector2Int goal)
-    {
+    private List<AStarNode> _open = new List<AStarNode>();
+    private List<AStarNode> _closed = new List<AStarNode>();
+    private List<AStarNode> _path = new List<AStarNode>();
 
+    private Vector2Int _start;
+    private Vector2Int _end;
+    private AStarNode _currentNode;
+    public List<AStarNode> Open => _open;
+    public List<AStarNode> Closed => _closed;
+    public List<AStarNode> Path => _path;
+    public AStarNode CurrentNode => _currentNode;
+    public int NbIterationsMax { get => _nbIterationsMax; set => _nbIterationsMax = value; }
+
+    public void GeneratePath(List<Vector2Int> map, Vector2Int start, Vector2Int end)
+    {
+        _open = new List<AStarNode>();
+        _closed = new List<AStarNode>();
+        _path = new List<AStarNode>();
+
+        _start = start;
+        _end = end;
+        if (!map.Contains(_start))
+            Debug.LogWarning("Start position " + _start + " is out of bounds");
+        if (!map.Contains(_end))
+            Debug.LogWarning("End position " + _end + " is out of bounds");
+
+        _currentNode = new AStarNode(_start, Vector2Int.Distance(_start, _end), 0, null);
+
+        int nbIterations = 0;
+
+        while (_currentNode.Position != _end && nbIterations < _nbIterationsMax)
+        {
+            foreach (var Neighbour in Neighbourhood.VonNeumann)
+            {
+                Vector2Int newPos = _currentNode.Position + Neighbour;
+                AStarNode newNode = new AStarNode(
+                    newPos,
+                    Vector2Int.Distance(newPos, _end) * _heuristicFactor,
+                    _currentNode.C + 1,
+                    _currentNode);
+
+                if (map.Contains(newPos) && !_open.Exists(Neighbour => Neighbour.Position == newPos) && !._closed.Exists(Neighbour => Neighbour.Position == newPos))
+                    _open.Add(newNode);
+            }
+            _currentNode = _open.OrderBy(o => o.S).ElementAt(0);
+            _closed.Add(_currentNode);
+            _open.Remove(_currentNode);
+            nbIterations++;
+        }
+
+        AStarNode rollbacknode = new AStarNode(_currentNode);
+        while (rollbacknode.Position != _start)
+        {
+            rollbacknode = rollbacknode.Parent;
+            Path.Add(rollbacknode);
+        }
     }
 }
 
